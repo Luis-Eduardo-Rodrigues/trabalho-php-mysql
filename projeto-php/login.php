0d8324dcc0e4925557342ac28f0804e41a8ca0a5
Luis Eduardo Rodrigues <130309482+Luis-Eduardo-Rodrigues@users.noreply.github.com>
2024-11-06 07:38:54 -0300

    Add files via upload

---
+++ projeto-php/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style>body{ font-family: Poppins }</style>
+</head>
+<body>
+    <main class="h-screen w-full flex items-center justify-center">
+        <form action="salvar.php" method="post" class="flex items-center justify-center flex-col gap-8">
+            <div>
+                <h2 class="text-3xl">Login - SysFitness</h2>
+            </div>
+            <div>
+            <input class="w-72 py-2 px-4 border border-1 border-black rounded" type="text" placeholder="Usuário">
+            </div>
+            <div>
+                <input class="w-72 py-2 px-4 border border-1 border-black rounded" type="password" placeholder="Senha">
+            </div>
+            <div>
+                <button type="submit" class="py-2 px-4 bg-violet-600 text-white rounded">Entrar</button>
+            </div>
+            <div>
+                <p>Não tem uma conta? <a href="cadastrar.php" class="font-bold text-violet-600">Criar conta</a></p>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
